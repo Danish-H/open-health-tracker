12fcc53acf9c0da817ea62a7cf499d3bf04cff2d
Gradyn Wursten <me@gradyn.com>
2023-02-19 13:28:05 -0700

    change title

---
+++ src/Components/emoji-button.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class EmojiButton extends Component {
+    render() {
+        return (
+            <div className="emoji-button">
+                
+            </div>
+        )
+    }
+}
