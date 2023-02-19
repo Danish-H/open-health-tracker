272ddadba3343e5e57dd04dedf7f73ae52a5a842
Gradyn Wursten <me@gradyn.com>
2023-02-18 18:55:49 -0700

    fix headear color

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <div id="header">
-                <h1><span class="gray">Hello</span> {this.props.name}!</h1>
+                <h1>Hello <span>{this.props.name}</span>!</h1>
                 <img id="profile" src={this.props.pfp} alt="profile"/>
             </div>
         )
