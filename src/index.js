179161567bc0f3638d4b0f9f069c8d6e4ec7b2ce
Gradyn Wursten <me@gradyn.com>
2023-02-19 16:42:56 -0700

    Revert "remove redundant root"
    This reverts commit 47c1c35dd8292362c3e14ed0dc026d497bbdb8d7.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const body = ReactDOM.createRoot(document.body);
-body.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
