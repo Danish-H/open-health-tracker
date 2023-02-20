6aedb90b58c9d13e4200a0c024b1d70caa8118bc
Danish-H <me@danishhumair.com>
2023-02-20 05:43:13 +0500

    feat emoji-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
   import './App.css';
   import Card from './Components/card'
   import Header from './Components/header'
+  import EmojiButton from './Components/emoji-button';
 
   function App() {
     return (
@@ -10,7 +11,14 @@
           <p>stuff here</p>
         </Card>
         <Card bgcolor="#DDF6D7" title="How's it going?" showbg>
-          <p>stuff here</p>
+          <div>
+            {/* These are just examples */}
+            <EmojiButton src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f604.svg"></EmojiButton>
+            <EmojiButton src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f605.svg"></EmojiButton>
+            <EmojiButton src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f606.svg"></EmojiButton>
+            <EmojiButton src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f607.svg"></EmojiButton>
+            <EmojiButton src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f608.svg"></EmojiButton>
+          </div>
         </Card>
         <Card bgcolor="#D7EBF6" title="Medication Tracker" showbg>
           <p>stuff here</p>
