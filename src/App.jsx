f7ad7c92d4ee56b312fe5c45f343fe2c975ce222
Gradyn Wursten <me@gradyn.com>
2023-02-20 11:20:29 -0700

    switch to a different authentication library

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-  import './App.css';
-  import Card from './Components/card'
-  import Header from './Components/header'
-  import { useAuth } from 'oidc-react';
+import './App.css';
+import Card from './Components/card'
+import Header from './Components/header'
 
-  function App() {
-    console.log(useAuth())
-    return (
-      <div>
-        <Header name="test" pfp="profile.svg"/>
-        <Card bgcolor="#eee" title="This Week in a Glance">
-          <p>stuff here</p>
-        </Card>
-        <Card bgcolor="#DDF6D7" title="How's it going?" showbg>
-          <p>stuff here</p>
-        </Card>
-        <Card bgcolor="#D7EBF6" title="Medication Tracker" showbg>
-          <p>stuff here</p>
-        </Card>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header name="test" pfp="profile.svg"/>
+      <Card bgcolor="#eee" title="This Week in a Glance">
+        <p>stuff here</p>
+      </Card>
+      <Card bgcolor="#DDF6D7" title="How's it going?" showbg>
+        <p>stuff here</p>
+      </Card>
+      <Card bgcolor="#D7EBF6" title="Medication Tracker" showbg>
+        <p>stuff here</p>
+      </Card>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from 'oidc-react';
+import { OidcProvider } from '@axa-fr/react-oidc';
 
+const oidc_configuration = {
+  client_id: 'frontend',
+  redirect_uri: window.location.origin + '/authentication/callback',
+  scope: 'openid profile email api offline_access',
+  authority: 'https://sso.hbigroup.org/realms/openhealthtracker',
+  service_worker_relative_url:'/OidcServiceWorker.js',
+  service_worker_only:true,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <AuthProvider
-        authority="https://sso.hbigroup.org/realms/openhealthtracker"
-        clientId="frontend"
-        redirectUri="http://localhost:3000/login-uri"
-        >
+        <OidcProvider configuration={oidc_configuration} >
           <App />
-        </AuthProvider>
+        </OidcProvider>
   </React.StrictMode>
 );
 
