49dccd3b832e3bcb49b15d51fea71e4462ac8fe3
Gradyn Wursten <me@gradyn.com>
2023-02-20 11:57:07 -0700

    move config to seperate file

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { OidcProvider, OidcSecure } from '@axa-fr/react-oidc';
-
-const oidc_configuration = {
-  client_id: 'frontend',
-  redirect_uri: window.location.origin + '/authentication/callback',
-  scope: 'openid profile email offline_access',
-  authority: 'https://sso.hbigroup.org/realms/openhealthtracker',
-  service_worker_relative_url:'/OidcServiceWorker.js',
-  service_worker_only:true,
-};
+import { oidc_configuration } from './config.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
