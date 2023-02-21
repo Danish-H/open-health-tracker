d50aac18d5b395a500aba31e98713506b213aafb
Gradyn Wursten <me@gradyn.com>
2023-02-21 16:18:11 -0700

    put teh div back

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,11 @@ function App() {
               <p>stuff here</p>
             </Card>
             <Card bgcolor="#DDF6D7" title="How's it going?" showbg>
+              <div>
               {summary.emotions.map(x => {
                 return <EmojiButton src={"https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/" + x.icon + ".svg"}/>
               }).reverse()}
+              </div>
             </Card>
             <Card bgcolor="#D7EBF6" title="Medication Tracker" showbg>
               <p>stuff here</p>
