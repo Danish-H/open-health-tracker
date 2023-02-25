4bbd66b0bd47fb4b2713a6a0ecf1a12e849340d2
Gradyn Wursten <me@gradyn.com>
2023-02-24 17:09:39 -0700

    fix pointer

--- src/Components/Slideup.jsx
+++ src/Components/Slideup.jsx
@@ -3,7 +3,7 @@ import "./Slideup.css"
 export default function Slideup ({onClose = () => {}, shown = false}) {
     return (
         <div className={!shown ? "slideup" : "slideup open"}>
-            <span onClick={onClose  } className="expand"></span>
+            <span onClick={onClose} className="expand"></span>
             
         </div>  
     )
