d4af72cdf628f187f14f675a26d875b3bb5713bb
Gradyn Wursten <me@gradyn.com>
2023-02-25 15:52:08 -0700

    borked code

--- src/Components/emotion-button.jsx
+++ src/Components/emotion-button.jsx
@@ -1,3 +1,9 @@
+import "./emotion-button.css"
+
 export default function EmotionButton(props) {
-    
+    return (
+        <div className="emotion-button">
+            <img src={props.icon}></img>
+            <p>{props.name}</p>
+        </div>)
 }
--- src/Components/emotion-category.jsx
+++ src/Components/emotion-category.jsx
@@ -1,9 +1,15 @@
+import EmotionButton from './emotion-button'
 import './emotion-category.css'
 
 export default function EmotionCategory({name = "", emotions = []}) {
     return (
         <div className="emotion-category">
             <h3>{name}</h3>
+            <div>
+                {emotions.map(x => {
+                    return <EmotionButton name={x.name} icon={"https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/" + x.icon + ".svg"}></EmotionButton>
+                })}
+            </div>
         </div>
     )
 }
