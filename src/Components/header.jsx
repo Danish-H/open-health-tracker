d5d95e2565838e8c8903b37fdc5f1dd8d1fc6c1c
Gradyn Wursten <me@gradyn.com>
2023-02-26 10:44:45 -0700

    allow togglign profile menu

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,14 +1,16 @@
 import './header.css'
 import { useOidc } from '@axa-fr/react-oidc'
+import React, { useState } from 'react';
 
 export default function Header (props) {
+    const [profileMenu, setProfileMenu] = useState(false);
     const { logout } = useOidc();
     return (
         <div id="header">
             <h1>Hello <span>{props.name}</span>!</h1>
-            <img id="user" src={props.pfp} alt="user" /* add onclick event here that adds .open to <ul> */ />
-            <ul class="open">
-                <div class="background" /* add onclick event here that removes .open from <ul> */ ></div>
+            <img id="user" src={props.pfp} alt="user" onClick={() => {setProfileMenu(true)}} />
+            <ul className={profileMenu? "open" : ""}>
+                <div class="background" onClick={() => {setProfileMenu(false)}} ></div>
                 <li id="profile"><img src="profile.svg" alt="profile"/> Profile</li>
                 <li id="settings"><img src="settings.svg" alt="profile"/> Settings</li>
                 <li id="signout" onClick={logout}><img src="signout.svg" alt="profile"/> Sign Out</li>
