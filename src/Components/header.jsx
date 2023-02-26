3e84ca2173e5cb429f090bc1ec4b12742975c9b3
Danish-H <me@danishhumair.com>
2023-02-26 15:15:55 +0500

    stuff

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -6,9 +6,9 @@ export default function Header (props) {
     return (
         <div id="header">
             <h1>Hello <span>{props.name}</span>!</h1>
-            <img id="user" src={props.pfp} alt="user" /* add onclick event here which adds .open to <div class="entire-background"></div> AND <ul>...</ul> */ />
-            <div class="background"></div> {/* clicking this div should remove .open from both this div and ul */}
-            <ul class="open">
+            <img id="user" src={props.pfp} alt="user" /* add onclick event here that adds .open to <ul> */ />
+            <ul>
+                <div class="background" /* add onclick event here that removes .open from <ul> */ ></div>
                 <li id="profile"><img src="profile.svg" alt="profile"/> Profile</li>
                 <li id="settings"><img src="settings.svg" alt="profile"/> Settings</li>
                 <li id="signout" onClick={logout}><img src="signout.svg" alt="profile"/> Sign Out</li>
