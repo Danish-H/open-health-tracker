5537258bfc5857d730ffd2cc23dbd7effdc17667
Gradyn Wursten <me@gradyn.com>
2023-02-25 19:37:44 -0700

    refractoring

--- src/Components/emotion-button.jsx
+++ src/Components/emotion-button.jsx
@@ -2,7 +2,7 @@ import "./emotion-button.css"
 
 export default function EmotionButton({name, icon, selected = false, id, onClick = () => {}}) {
     return (
-        <div onClick={() => {onClick()}} className={!selected ? "emotion-button" : "emotion-button selected"}>
+        <div onClick={onClick} className={!selected ? "emotion-button" : "emotion-button selected"}>
             <img alt={name} src={icon}></img>
             <span>{name}</span>
         </div>)
