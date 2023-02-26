e2306b2837596a0a7fd569119631d19ad534a42b
Danish-H <me@danishhumair.com>
2023-02-26 13:46:13 +0500

    some code cleanup

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ export const oidc_configuration = {
     authority: 'https://sso.hbigroup.org/realms/openhealthtracker',
     service_worker_relative_url:'/OidcServiceWorker.js',
     service_worker_only:false,
-  };
+};
 
 export const api_configuration = {
-  api_root: "https://oht.gradyn.com/api"
+    api_root: "https://oht.gradyn.com/api"
 }
