f7e068c85da07a11e3157083b2f5efb52ca5b0d7
Danish-H <me@danishhumair.com>
2023-02-26 18:17:06 +0500

    fixed CSS issues with profile menu

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -7,7 +7,7 @@ export default function Header (props) {
         <div id="header">
             <h1>Hello <span>{props.name}</span>!</h1>
             <img id="user" src={props.pfp} alt="user" /* add onclick event here that adds .open to <ul> */ />
-            <ul>
+            <ul class="open">
                 <div class="background" /* add onclick event here that removes .open from <ul> */ ></div>
                 <li id="profile"><img src="profile.svg" alt="profile"/> Profile</li>
                 <li id="settings"><img src="settings.svg" alt="profile"/> Settings</li>
