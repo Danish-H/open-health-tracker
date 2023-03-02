db6311ea2f417cb7b9657056b18678979e4a69af
Gradyn Wursten <me@gradyn.com>
2023-03-02 14:21:12 -0700

    update icon url format

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,10 +1,11 @@
 import './header.css'
-import { useOidc } from '@axa-fr/react-oidc'
+import { OidcUserStatus, useOidc } from '@axa-fr/react-oidc'
 import React, { useState } from 'react';
-
+b
 export default function Header (props) {
     const [profileMenu, setProfileMenu] = useState(false);
     const { logout } = useOidc();
+
     return (
         <div id="header">
             <h1>Hello <span>{props.name}</span>!</h1>
@@ -12,6 +13,7 @@ export default function Header (props) {
             <ul className={profileMenu? "open" : ""}>
                 <div class="background" onClick={() => {setProfileMenu(false)}} ></div>
                 <li onClick={() => {window.location = "https://sso.hbigroup.org/realms/openhealthtracker/account/"}} id="profile"><img src="profile.svg" alt="profile"/> Profile</li>
+                {/* {oidcUserLoadingState === OidcUserStatus.Loaded && oidcUser} */}
                 <li id="settings"><img src="settings.svg" alt="profile"/> Settings</li>
                 <li id="signout" onClick={logout}><img src="signout.svg" alt="profile"/> Sign Out</li>
             </ul>
