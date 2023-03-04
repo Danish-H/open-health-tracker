a998eb9d076c0fe007abc0e0650bba7a8ad5850a
Danish-H <me@danishhumair.com>
2023-03-05 01:22:00 +0500

    fixed activities css

--- src/Components/activity-button.jsx
+++ src/Components/activity-button.jsx
@@ -3,7 +3,7 @@ import './activity-button.css'
 export default function Activity ({name, icon}) {
     return (
         <div className="activity-button">
-            <img src={icon}></img>
+            <div><img src={icon}></img></div>
             <span>{name}</span>
         </div>
     )
