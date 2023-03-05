88276983d76adf61ac4c06c0e42d515d5d245192
Danish-H <me@danishhumair.com>
2023-03-05 19:25:45 +0500

    unnecessary change in pill size

--- src/Components/pill-menu.jsx
+++ src/Components/pill-menu.jsx
@@ -1,6 +1,6 @@
 import "./pill-menu.css"
 
-export default function PillMenu({primaryLabel = "Save", secondaryLabel = "Cancel", primaryColor = "#caead3", secondaryColor = "#252525", primaryTextColor = "#252525", secondaryTextColor = "#888888", dropShadow = false, primaryClick, secondaryClick}) {
+export default function PillMenu({primaryLabel = "Save", secondaryLabel = "Cancel", primaryColor = "#caead3", secondaryColor = "#252525", primaryTextColor = "#252525", secondaryTextColor = "#aaaaaa", dropShadow = false, primaryClick, secondaryClick}) {
     return (
         <div className={dropShadow ? "pill-menu drop-shadow" : "pill-menu"} style={{backgroundColor: secondaryColor, color: secondaryTextColor}}>
             <span onClick={secondaryClick}>{secondaryLabel}</span>
